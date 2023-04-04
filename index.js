071b46ba64b728a0b7d6f6dbdc28a1d06d55524e
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-04-04 23:11:08 +0530

    index.js with a few changes added.

--- index.js
+++ index.js
@@ -10,8 +10,33 @@ app.get("/", (req, res)=>{
   res.send("<h1>hello full stack web dev JavaScript</h1>");
 });
 
-app.get("/insta", ()=>{
-  res.status(404).send("Hello World");
+app.get("/insta", (req, res)=>{
+  // res.status(404).send("Hello World");
+  const insta={
+    userName: "vyompundhir",
+    follow: 1,
+    follower: 20,
+  };
+  res.status(200).send({insta});
+});
+
+app.get("/linkedin", (req, res)=>{
+  const linkedin={
+    userName: "vyompundhir",
+    follow: 14,
+    follower: 24,
+  };
+  res.status(200).json({linkedin});
+  // we can write "json" in place of "send"..
+});
+
+app.get("/twitter", (req, res)=>{
+  const twitter={
+    userName: "vyompundhir",
+    follow: 40,
+    follower: 24,
+  };
+  res.status(200).send({twitter});
 });
 
 // listen 
